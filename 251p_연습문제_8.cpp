43f9bf88c3a1daf39cefbb8839a61be2f4619bce
YunTaeHui <yth6869@gmail.com>
2025-10-15 22:01:29 +0900

    Add files via upload

--- 251p_연습문제_8.cpp
+++ 251p_연습문제_8.cpp
@@ -1,29 +1,61 @@
+//251p_연습문제_8
+//#include <stdio.h>
+//
+//void main()
+//{
+//    char str[100];
+//    char ch;
+//
+//    int i, k;
+//    int star;
+//
+//    printf("숫자를 여러 개 입력 : ");
+//    scanf("%s", str);
+//
+//    i = 0;
+//    ch = str[i];
+//    while (ch != '\0') {
+//        star = (int)ch - 48;
+//
+//        k = 0;
+//        while (k < star * 2)
+//        {
+//            printf("\u2665");
+//            k++;
+//        }
+//        printf("\n");
+//        i++;
+//        ch = str[i];
+//    }
+//}
+
 #include <stdio.h>
 
-void main()
+int main(void)
 {
-	char str[100];
-	char ch;
+    int num;
+    printf("숫자를 여러 개 입력 : ");
+    scanf("%d", &num);
 
-	int i, k;
-	int star;
+    int max10 = 1;
+    int imsi = num;
+    while (imsi >= 10) {
+        imsi /= 10;
+        max10 *= 10;
+    }
 
-	printf("숫자를 여러 개 입력 : ");
-	scanf("%s", str);
+    while (max10 > 0) {
+        int digit = num / max10;
+        int heart = 0;
+        while (heart < digit) {
+            printf("\u2665");
+            heart++;
+        }
+        printf("\n");
 
-	i = 0;
-	ch = str[i];
-	while (ch != '\0') {
-		star = (int)ch - 48;
+        num %= max10;
+        max10 /= 10;
+    }
 
-		k = 0;
-		while (k < star * 2)
-		{
-			printf("\u2665");
-			k++;
-		}
-		printf("\n");
-		i++;
-		ch = str[i];
-	}
+    return 0;
 }
